118fe41091266c061e66d52e648a1d0d05c493ea
mitrukahitesh <mitrukahitesh@gmail.com>
2021-09-11 09:16:58 +0530

    initial

---
+++ app/src/main/java/com/skywalkers/cosapa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.skywalkers.cosapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
