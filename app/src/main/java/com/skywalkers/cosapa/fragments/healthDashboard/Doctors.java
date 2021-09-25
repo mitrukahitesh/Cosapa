465f61004257cd25faaf1f8d0996915c735abee3
mitrukahitesh <mitrukahitesh@gmail.com>
2021-09-25 11:32:24 +0530

    finalised doctors list ui with filter

--- app/src/main/java/com/skywalkers/cosapa/fragments/healthDashboard/Doctors.java
+++ app/src/main/java/com/skywalkers/cosapa/fragments/healthDashboard/Doctors.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.skywalkers.cosapa.R;
 import com.skywalkers.cosapa.adapters.DoctorAdapter;
@@ -21,6 +25,14 @@ import com.skywalkers.cosapa.adapters.DoctorAdapter;
 public class Doctors extends Fragment {
 
     private RecyclerView recyclerView;
+    private NavController controller;
+    private CardView cardView1, cardView2, cardView3, cardView4;
+    private TextView cardText1, cardText2, cardText3, cardText4;
+    public static final String ALL = "";
+    public static final String DERMATOLOGY = "DERMATOLOGY";
+    public static final String FAM_MED = "FAMILY MEDICINE";
+    public static final String ENT = "ENT";
+    private ConstraintLayout root;
 
     public Doctors() {
     }
@@ -45,9 +57,80 @@ public class Doctors extends Fragment {
                 Navigation.findNavController(view).popBackStack();
             }
         });
+        controller = Navigation.findNavController(view);
+        cardView1 = view.findViewById(R.id.card1);
+        cardView2 = view.findViewById(R.id.card2);
+        cardView3 = view.findViewById(R.id.card3);
+        cardView4 = view.findViewById(R.id.card4);
+        cardText1 = view.findViewById(R.id.cardText1);
+        cardText2 = view.findViewById(R.id.cardText2);
+        cardText3 = view.findViewById(R.id.cardText3);
+        cardText4 = view.findViewById(R.id.cardText4);
+        setCardListeners();
         recyclerView = view.findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setNestedScrollingEnabled(false);
-        recyclerView.setAdapter(new DoctorAdapter(getContext(), "", view.findViewById(R.id.root)));
+        root = view.findViewById(R.id.root);
+        recyclerView.setAdapter(new DoctorAdapter(getContext(), ALL, root, controller));
+    }
+
+    private void setCardListeners() {
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDefaultFilterColors();
+                if (getActivity() == null)
+                    return;
+                cardView1.setCardBackgroundColor(getActivity().getColor(R.color.col2));
+                cardText1.setTextColor(getActivity().getColor(R.color.white));
+                recyclerView.setAdapter(new DoctorAdapter(getContext(), ALL, root, controller));
+            }
+        });
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDefaultFilterColors();
+                if (getActivity() == null)
+                    return;
+                cardView2.setCardBackgroundColor(getActivity().getColor(R.color.col2));
+                cardText2.setTextColor(getActivity().getColor(R.color.white));
+                recyclerView.setAdapter(new DoctorAdapter(getContext(), DERMATOLOGY, root, controller));
+            }
+        });
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDefaultFilterColors();
+                if (getActivity() == null)
+                    return;
+                cardView3.setCardBackgroundColor(getActivity().getColor(R.color.col2));
+                cardText3.setTextColor(getActivity().getColor(R.color.white));
+                recyclerView.setAdapter(new DoctorAdapter(getContext(), FAM_MED, root, controller));
+            }
+        });
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDefaultFilterColors();
+                if (getActivity() == null)
+                    return;
+                cardView4.setCardBackgroundColor(getActivity().getColor(R.color.col2));
+                cardText4.setTextColor(getActivity().getColor(R.color.white));
+                recyclerView.setAdapter(new DoctorAdapter(getContext(), ENT, root, controller));
+            }
+        });
+    }
+
+    private void setDefaultFilterColors() {
+        if (getActivity() == null)
+            return;
+        cardView1.setCardBackgroundColor(getActivity().getColor(R.color.white));
+        cardView2.setCardBackgroundColor(getActivity().getColor(R.color.white));
+        cardView3.setCardBackgroundColor(getActivity().getColor(R.color.white));
+        cardView4.setCardBackgroundColor(getActivity().getColor(R.color.white));
+        cardText1.setTextColor(getActivity().getColor(R.color.black));
+        cardText2.setTextColor(getActivity().getColor(R.color.black));
+        cardText3.setTextColor(getActivity().getColor(R.color.black));
+        cardText4.setTextColor(getActivity().getColor(R.color.black));
     }
 }
