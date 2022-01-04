fcf8a0b18b99cdedaf0dbfdde3ca49b07f83500a
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2022-01-05 01:45:45 +0530

    localisation done

--- app/src/main/java/com/skywalkers/cosapa/MainActivity.java
+++ app/src/main/java/com/skywalkers/cosapa/MainActivity.java
@@ -139,4 +139,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleHelper.onAttach(base));
+    }
 }
